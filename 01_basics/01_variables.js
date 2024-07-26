9dac3030c2025d63121298f2a9adb6a2afdf4d8b
AgentKaash <162159386+AgentKaash@users.noreply.github.com>
2024-07-26 02:44:00 +0000

    variables and constant done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail = "kaashlai@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+console.log(accountId)
+
+accountEmail = "k@k@.com"
+accountPassword ="21212121"
+accountCity ="bangaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState])
