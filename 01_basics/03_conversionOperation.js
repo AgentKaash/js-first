a2982212dcd0403578a9534b1176c797bec949e1
AgentKaash <162159386+AgentKaash@users.noreply.github.com>
2024-07-27 11:12:07 +0000

    operators and prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,11 +13,44 @@ let valueInNumber = Number(score)
 
 let isLoggedIn ="kl"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 -->true ; 0 --> false
 //""-->false
 //"kl"-->true
 
+// ********** Operation *********
+let value = 3
+let negValue = -value
+// 
+// console.log(2+2)
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 =" kaash"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+//prefix and postfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
