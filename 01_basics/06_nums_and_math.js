a6af00550d056cdbe21d8321e046354391789e97
AgentKaash <162159386+AgentKaash@users.noreply.github.com>
2024-07-31 05:33:00 +0000

    math or num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++ Maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs)(-4);
+// console.log(Math.round(4.6));
+// console.log(Math.celi(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,5,8));
+
+console.log(Math.random()); // For any random value eg:flote  value
+
+console.log(Math.random()*10); // random value multiplied by 10
+console.log((Math.random()*10)+1); // For random value not zero
+console.log(Math.floor(Math.random()*10)+1); // for random  digit
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)+ min)); //To specify the value b/w them
+
+
+
+
