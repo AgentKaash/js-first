c9ed2cdb55ffa6767026358f91c48166989e3d0d
AgentKaash <162159386+AgentKaash@users.noreply.github.com>
2024-08-01 05:59:12 +0000

    date

--- 01_basics/07_dateinjs.js
+++ 01_basics/07_dateinjs.js
@@ -1,11 +1,18 @@
 //Dates ---->object
 
 let myDate = new Date()
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleString());
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
 
+// let myCreateDate = new Date(2024, 6, 31)
+// let myCreateDate = new Date(2024, 6, 31, 23, 18)
+let myCreateDate = new Date("2024-7-31")
+
+console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp
